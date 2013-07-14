46ab37014fdbad78a381598f4aca3b616828c7db
mpoiriert <mpoiriert@gmail.com>
2013-07-14 19:22:44 -0400

    [+] simplify the request/response handle since the frontcontroller now return the response

--- src/Nucleus/Cell/Kohana/KohanaMutation.php
+++ src/Nucleus/Cell/Kohana/KohanaMutation.php
@@ -12,9 +12,7 @@ use Nucleus\DependencyInjection\BaseAspect;
 use Nucleus\Routing\Router;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Route;
-use Nucleus\IService\Invoker\IInvokerService;
 use Symfony\Component\HttpFoundation\Request as NucleusRequest;
-use Symfony\Component\HttpFoundation\Response as NucleusResponse;
 
 /**
  * Description of KohanaAspect
@@ -74,47 +72,24 @@ class KohanaMutation extends BaseAspect
         
         $request->request->add($kohanaParameters['_nucleus']);
         
-        $parameters = $kohanaParameters['_nucleus'];
-        $serviceName = $parameters['_service']['name'];
-        $methodName = $parameters['_service']['method'];
+        $service = $request->request->get('_service');
         
-        $response = new NucleusResponse();
-        $service = $this->getServiceContainer()->getServiceByName($serviceName);
-        $executionResult = $this->getInvoker()->invoke(
-            array($service, $methodName), 
-            array_merge($request->query->all(), $request->request->all()),
-            array($request, $response)
-        );
-        
-        $result = array('result' => $executionResult);
-        
-        $this->completeResponse($request, $response, $result);
-        $response->prepare($request);
-        
+        $response = $this->getFrontController()
+            ->execute($service['name'], $service['method'], $request);
+
         $kohanaResponse = $kohanaRequest->create_response();
         $kohanaResponse->body($response->getContent());
         $kohanaResponse->headers($response->headers->all());
         return $kohanaResponse;
     }
     
-    private function completeResponse(NucleusRequest $request, NucleusResponse $response, $result)
-    {
-        foreach ($request->getAcceptableContentTypes() as $contentType) {
-            foreach ($this->getServiceContainer()->getServicesByTag('responseAdapter') as $adapter) {
-                if ($adapter->adaptResponse($contentType, $request, $response, $result)) {
-                    $response->headers->set('Content-Type', $contentType);
-                    return;
-                }
-            }
-        }
-    }
-    
+   
     /**
-     * @return IInvokerService
+     * @return \Nucleus\FrontController\FrontController
      */
-    private function getInvoker()
+    private function getFrontController()
     {
-        return $this->getServiceContainer()->getServiceByName(IInvokerService::NUCLEUS_SERVICE_NAME);
+        return $this->getServiceContainer()->getServiceByName('frontController');
     }
     
     /**
