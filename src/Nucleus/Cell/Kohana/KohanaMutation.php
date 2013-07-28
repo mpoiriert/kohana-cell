b8a7842fed0199e2039d2dc66b4dc3b89c1c06d8
mpoiriert <mpoiriert@gmail.com>
2013-07-28 17:21:31 -0400

    [+] support of 2 way route

--- src/Nucleus/Cell/Kohana/KohanaMutation.php
+++ src/Nucleus/Cell/Kohana/KohanaMutation.php
@@ -13,6 +13,9 @@ use Nucleus\Routing\Router;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Route;
 use Symfony\Component\HttpFoundation\Request as NucleusRequest;
+use Request as KohanaRequest;
+use Nucleus\IService\EventDispatcher\IEventDispatcherService;
+use ArrayObject;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 /**
@@ -36,10 +39,16 @@ class KohanaMutation extends BaseAspect
             $result = $this->getRouter()->match('/' . $arguments[0]);
             $route = new Route($arguments[0]);
             $route->defaults($result);
-            return array(
-                'params' => array('controller'=>'nucleus','action'=>'execute','_nucleus'=>$result),
-                'route' => $route
+            $params = new ArrayObject(
+                array(
+                    'params' => array('controller'=>'nucleus','action'=>'execute','_nucleus'=>$result),
+                    'route' => $route
+                )
             );
+
+            $this->getEventDispatcher()->dispatch('KohanaMutation.processUri',$params);
+    
+            return $params->getArrayCopy();
         } catch (ResourceNotFoundException $e) {
             return $invocation->proceed();
         }
@@ -48,17 +57,38 @@ class KohanaMutation extends BaseAspect
     /**
      * @param MethodInvocation $invocation Invocation
      *
-     * @Go\Lang\Annotation\Around("execution(public Kohana_Route::url(*))")
+     * @Go\Lang\Annotation\Around("execution(public Nucleus\Routing\Router->addRoute(*))")
      */
-    public function aroundKohanaRouteUrl(MethodInvocation $invocation)
+    public function beforeAddRoute(MethodInvocation $invocation)
+    {
+        list($name, $path, $defaults, $requirements, $options, $host, $schemes, $methods) = $invocation->getArguments();
+       
+        if(isset($defaults['_culture']) && $defaults['_culture']) {
+            $name = $defaults['_culture'] . ':i18n:' . $name;
+        }
+        
+        Route::set($name, $path)->defaults($defaults);
+        
+        $invocation->proceed();
+    }
+    
+    /**
+     * @param MethodInvocation $invocation Invocation
+     *
+     * @Go\Lang\Annotation\Around("execution(public Kohana_Route->uri(*))")
+     */
+    public function aroundKohanaRouteGet(MethodInvocation $invocation)
     {
         $arguments = $invocation->getArguments();
+        $route = $invocation->getThis();
+        $name = Route::name($route);
+        
+        $parameters = $arguments[0];
+        if(empty($parameters)) {
+            $parameters = array();
+        }
+        
         try {
-            //Third argument is protocole but we do nothing with it for now
-            list($name, $parameters) = $arguments;
-            if(is_null($parameters)) {
-                $parameters = array();
-            }
             return $this->getRouter()->generate($name, $parameters);
         } catch (RouteNotFoundException $e) {
             return $invocation->proceed();
@@ -80,7 +110,11 @@ class KohanaMutation extends BaseAspect
         if(!isset($kohanaParameters['_nucleus'])) {
             return $invocation->proceed();
         }
-       
+
+        $previousKohanaRequest = KohanaRequest::$current;
+        
+        KohanaRequest::$current = $kohanaRequest;
+        
         $request = new NucleusRequest(
             $kohanaRequest->query(),
             $kohanaRequest->post(),
@@ -94,16 +128,24 @@ class KohanaMutation extends BaseAspect
         $request->request->add($kohanaParameters['_nucleus']);
         
         $service = $request->request->get('_service');
-        
+           
         $response = $this->getFrontController()
             ->execute($service['name'], $service['method'], $request);
 
         $kohanaResponse = $kohanaRequest->create_response();
         $kohanaResponse->body($response->getContent());
         $kohanaResponse->headers($response->headers->all());
+        KohanaRequest::$current = $previousKohanaRequest;
         return $kohanaResponse;
     }
     
+    /**
+     * @return IEventDispatcherService
+     */
+    private function getEventDispatcher()
+    {
+        return $this->getServiceContainer()->getServiceByName(IEventDispatcherService::NUCLEUS_SERVICE_NAME);
+    }
    
     /**
      * @return \Nucleus\FrontController\FrontController
